cea3478a43cebe26b971a6142645a339f8991b4e
nikkiwritescode <nicole.farris@outlook.com>
2021-06-08 17:08:38 -0500

    Added support for logging to file with optional parameter -o and colorized the output of the relevant values in the terminal.

--- filediff.py
+++ filediff.py
@@ -1,14 +1,28 @@
 import argparse
 import sys
+import os
+import pathlib
 firstinputfile = ""
 secondinputfile = ""
+outputfile = ""
+log_file = ""
 
 parser = argparse.ArgumentParser(
     description='Compare two files line-by-line for differences.'
 )
 parser.add_argument('-f1', '--file1', help='the first input file')
 parser.add_argument('-f2', '--file2', help='the second input file')
+parser.add_argument('-o', '--output', help='(optional) the output file name')
 args = parser.parse_args()
+
+outputfile = args.output
+if(os.path.isdir(pathlib.Path(outputfile).parent.absolute()) is False):  # if the referenced log folder does not exist
+    os.makedirs(pathlib.Path(outputfile).parent.absolute(), mode=0o755, exist_ok=False)  # create it
+
+if(os.path.isfile(args.file1) is False):  # if the log file does not exist
+    log_file = open(outputfile, "x")  # create it
+elif(os.path.isfile(args.file1) is True):  # but f it does exist,
+    log_file = open(outputfile, "a")  # append to it
 
 
 def main(argv):
@@ -16,13 +30,13 @@ def main(argv):
     diffCount = 0
     sameCount = 0
 
-    if(args.file1 is None):  # if its the default value
+    if(os.path.isfile(args.file1) is False):  # if its the default value
         ShowFileMissingError()
         sys.exit(2)
     else:
         firstinputfile = args.file1
 
-    if(args.file2 is None):
+    if(os.path.isfile(args.file2) is False):
         ShowFileMissingError()
         sys.exit(2)
     else:
@@ -30,39 +44,78 @@ def main(argv):
 
     try:
         with open(firstinputfile) as f1, open(secondinputfile) as f2:
-            print("---File Details---")
+            printing("---File Details---")
             for x, y in zip(f1, f2):
-                if(x == y):
-                    print(x.rstrip())  # if the lines match, print it
+                if(x == y):  # if the lines match, print it
+                    printing(x.rstrip(), fail=False, success=True)
                     sameCount += 1
-                else:
-                    print("File 1: " +
-                          f"Line {lineCount + 1}: {x.rstrip()} | ", end="")
-                    print("File 2: " +
-                          f"Line {lineCount + 1}: {y.rstrip()}")
+                else:  # and if they don't, print the data from both
+                    printing(f"Line {lineCount + 1}: ", no_newline=True)
+                    printing(f"File 1: ", no_newline=True)
+                    printing(x.rstrip(), no_newline=True, fail=True)
+                    printing(" | ", no_newline=True)
+                    printing(f"File 2: ", no_newline=True)
+                    printing(y.rstrip(), fail=True)
                     diffCount += 1
                 lineCount += 1
+        printing("------Results------")
+        printing(f"The total line count was ", no_newline=True)
+        printing(f"{lineCount}.", no_newline=False, fail=False, success=True)
+        printing(f"There were ", no_newline=True)
+        printing(f"{diffCount}", no_newline=True, fail=True, success=False)
+        printing(" differing lines.")
+        printing(f"{sameCount}", no_newline=True, fail=False, success=True)
+        printing(" lines were identical between these files.")
+        printing('--------------------')
+        if(log_file != ""):
+            log_file.close()
     except FileNotFoundError:
         ShowFileNotFoundError()
         sys.exit(2)
 
-    print("------Results------")
-    print(f"The total line count was {lineCount}.")
-    print(f"There were {diffCount} differing lines.")
-    print(f"{sameCount} lines were identical between these files.")
-    print('--------------------')
-
 
 def ShowFileMissingError():
-    print("---Error Details---")
-    print("Please enter two files to compare! Use the -h flag for help.")
-    print('-------------------')
+    printing("---Error Details---")
+    printing("Please enter two files to compare! Use the -h flag for help.")
+    printing('-------------------')
 
 
 def ShowFileNotFoundError():
-    print("---Error Details---")
-    print('One or more of the specified input files does not exist!')
-    print('-------------------')
+    printing("---Error Details---")
+    printing('One or more of the specified input files does not exist!')
+    printing('-------------------')
+
+
+def printing(text, no_newline=False, fail=False, success=False):
+    if(success and fail):
+        print("Supplying both Fail and Success as true makes no sense!")
+        print("Exiting application.")
+        sys.exit(2)
+    if(no_newline):
+        print(ApplyFailOrSuccessColor(text, fail, success), end="")       
+    else:
+        print(ApplyFailOrSuccessColor(text, fail, success))  
+ 
+    if(log_file != ""):
+        if(no_newline):
+            log_file.write(text)
+        else:
+            log_file.write(text + "\n")
+
+
+def ApplyFailOrSuccessColor(text, fail, success):
+    if(fail):
+        return f"{bcolors.FAIL}{text}{bcolors.ENDC}"
+    elif(success):
+        return f"{bcolors.OKGREEN}{text}{bcolors.ENDC}"
+    else:
+        return text
+
+class bcolors:
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -1,8 +1,11 @@
-# Python File Differ
-This simple script compares two text files, line by line, for differences.
+# Simple File Differ
+This script compares two text files, line by line, for differences.
 
 ## How to Use
 From a terminal with Python 3 installed, you can use the following command to test the script with the included sample files: `python3 .\filediff.py -f1 ./sample/file1.txt -f2 ./sample/file2.txt`
 
 `-f1` or `--file1` specifies the location of the first input file.
 `-f2` or `--file2` specifies the location of the second input file.
+
+## Redirecting the output to a log file
+When running the script, add the `>` operator, followed by the path to the 
